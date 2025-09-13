9b02f4beb4a4626f0569bb77e26080cc2b95cc80
iamsahal77 <sahalsahsal2gmail@gmail.com>
2025-09-13 12:59:20 +0530

    fix logo

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ function createDots() {
         dots.push({
             x: Math.random() * canvas.width * 1.2 - canvas.width * 0.1,
             y: canvas.height + Math.random() * canvas.height,
-            radius: Math.random() * 0.5 + 0.54, // 0.6px to 1px size of dot
-            speed: (Math.random() * 0.7 + 0.6) * 0.2, // speed
+            radius: Math.random() * 0.5 + 0.54, 
+            speed: (Math.random() * 0.7 + 0.6) * 0.2, 
             color:
                 Math.random() < 0.9
                     ? "rgba(0, 0, 0, 1)"
                     : "rgba(128,128,128,0.95)",
-            drift: Math.random() * 0.6 - 0.4, // move left/right
+            drift: Math.random() * 0.6 - 0.4, 
         });
     }
 }
@@ -31,13 +31,11 @@ function drawDots() {
         ctx.fillStyle = dot.color;
         ctx.fill();
 
-        // Move dot upward + drifting sideways
         dot.y -= dot.speed;
         dot.x += dot.drift;
 
-        // Remove when reaching top
         if (dot.y < 0) {
-            dot.y = canvas.height + Math.random() * 50; // reset to bottom
+            dot.y = canvas.height + Math.random() * 50; 
             dot.x = Math.random() * canvas.width * 1.2 - canvas.width * 0.1;
         }
     }
@@ -51,7 +49,6 @@ function animate() {
 createDots();
 animate();
 
-// Resize canvas dynamically
 window.addEventListener("resize", () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
